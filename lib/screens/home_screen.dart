29e90d4baec425ed95c456896c8208cd8b560a26
Kali <abdallaayman190@gmail.com>
2021-08-01 13:44:19 -0400

    Finished pull-to-refresh feature to generate 5 random contacts data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jiffy/jiffy.dart';
@@ -14,6 +14,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // List users = [
+  //   'Abdalla Ayman',
+  //   'Ahmed Hassan',
+  //   'Shady Yasser',
+  //   'Mohammed Shahin',
+  //   'Nabila Mohammed'
+  // ];
+
+  // List phones = [
+  //   '0136737610',
+  //   '0134578945',
+  //   '0139789798',
+  //   '0167478941',
+  //   '0194571257'
+  // ];
+
+  // List checkins = [
+  //   '2019-06-30 16:10:05',
+  //   '2018-12-25 03:00:00',
+  //   '2021-03-01 14:30:05',
+  //   '2021-01-05 20:55:05',
+  //   '2020-09-16 18:20:05'
+  // ];
+
+  List contactList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,30 +53,72 @@ class _HomeScreenState extends State<HomeScreen> {
           builder: (contxt, snapshot) {
             if (snapshot.hasData) {
               if (snapshot.data != null) {
-                List contactList = json.decode(snapshot.data.toString());
+                if (contactList.isEmpty)
+                  contactList = json.decode(snapshot.data.toString());
                 sortList(contactList);
-                return ListView.separated(
-                  physics: BouncingScrollPhysics(),
-                  itemCount: contactList.length,
-                  separatorBuilder: (context, i) {
-                    return Divider(
-                      height: 5,
-                      thickness: 1.0,
-                    );
-                  },
-                  itemBuilder: (context, i) {
-                    var data = contactList[i];
-                    DateTime time = DateTime.parse(data['check-in']);
-                    return ListTile(
-                      title: Text(data['user']),
-                      subtitle: Text(data['phone']),
-                      trailing: Text(
-                        Jiffy(time).fromNow(),
-                        style: TextStyle(color: Colors.grey[700]),
+                return RefreshIndicator(
+                  onRefresh: () async {
+                    setState(() {
+                      for (int x = 0; x < 5; x++) {
+                        contactList.add(
+                          json.decode(
+                            '''{
+                                "user": "${faker.person.name()}",
+                                "phone": "${faker.phoneNumber.random.fromPattern([
+                              '01########'
+                            ])}",
+                                "check-in": "${faker.date.dateTime(minYear: 2015, maxYear: 2021)}"
+                              }''',
+                          ),
+                        );
+                      }
+
+                      print(contactList);
+                    });
+
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                        'Page Refreshed',
+                        textAlign: TextAlign.center,
                       ),
-                      leading: Icon(Icons.call),
-                    );
+                    ));
                   },
+                  child: ListView.separated(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: contactList.length + 1,
+                    separatorBuilder: (context, i) {
+                      return Divider(
+                        height: 5,
+                        thickness: 1.0,
+                      );
+                    },
+                    itemBuilder: (context, i) {
+                      if (i == contactList.length) {
+                        return Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 15.0),
+                            child: Text(
+                              '- You have reached the end of the list -',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                          ),
+                        );
+                      }
+                      var data = contactList[i];
+                      DateTime time = DateTime.parse(data['check-in']);
+                      return ListTile(
+                        title: Text(data['user']),
+                        subtitle: Text(data['phone']),
+                        trailing: Text(
+                          Jiffy(time).fromNow(),
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        leading: Icon(Icons.call),
+                      );
+                    },
+                  ),
                 );
               }
             }
